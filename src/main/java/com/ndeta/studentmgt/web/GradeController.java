a9ab47c069d5f24c0a764e596ea177cd7b5aba58
ndeta <ndetainnocent3@gmail.com>
2023-03-23 00:48:01 +0200

    Writing tests

--- src/main/java/com/ndeta/studentmgt/web/GradeController.java
+++ src/main/java/com/ndeta/studentmgt/web/GradeController.java
@@ -3,7 +3,6 @@ package com.ndeta.studentmgt.web;
 import com.ndeta.studentmgt.entity.Grade;
 import com.ndeta.studentmgt.service.GradeService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/test/java/com/ndeta/studentmgt/StudentmgtApplicationTests.java
+++ src/test/java/com/ndeta/studentmgt/StudentmgtApplicationTests.java
@@ -1,13 +1,20 @@
 package com.ndeta.studentmgt;
 
+import com.ndeta.studentmgt.entity.Course;
+import com.ndeta.studentmgt.repository.CourseRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@DataJpaTest
 @SpringBootTest
 class StudentmgtApplicationTests {
+    @Autowired
+    private CourseRepository courseRepository;
 
     @Test
-    void contextLoads() {
+    void testCreateCourse() {
+
     }
 
 }
